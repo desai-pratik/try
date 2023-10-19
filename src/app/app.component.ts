9384bb476a0d100e300b4973d1124dc76665fc34
pratik <pratikdesai2274@gmail.com>
2023-10-19 14:19:25 +0100

    day-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { UpperCasePipe } from '@angular/common';
+// import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,48 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipe';
-  public inputValue: any = '';
+  public inputValue: any = ''; 
 
-  transform(value: any) {
-      this.inputValue = value;
-  }
 
+  // search filter
   nameSearch:string = "";
+  selectSearch:any = "";
+  
   proname:any[] = [
-    {name:"pratik"},
-    {name:"Aarav"},
-    {name:"Aryan"},
-    {name:"Arjun"},
-    {name:"Akash"},
-    {name:"Siddharth"},
-    {name:"Rajat"},
-    {name:"Ankit"},
-    {name:"Aditya"},
-    {name:"Abhinav"},
-    {name:"Vikram"},
-    {name:"Rahul"},
-    {name:"Sameer"},
-    {name:"Karan"},
-    {name:"Deepak"},
-    {name:"Aishwarya"},
-    {name:"Neha"},
-    {name:"Priya"},
-    {name:"Pooja"},
-    {name:"Swati"},
-    {name:"Divya"},
-    {name:"Aaradhya"},
-    {name:"Sneha"},
-    {name:"Kavita"},
-    {name:"Roshni"},
-    {name:"Shruti"},
-    {name:"Tanvi"},
-    {name:"Gauri"},
-    {name:"Meera"},
-    {name:"Nisha"},
+    {name:"pratik" , age:20 , Hsc: 48, gender: "male"},
+    {name:"Aarav" , age:25 , Hsc: 20, gender: "male"},
+    {name:"Aryan" , age:98 , Hsc: 68, gender: "male"},
+    {name:"Arjun" , age:15 , Hsc: 94, gender: "male"},
+    {name:"Akash" , age:65 , Hsc: 37, gender: "male"},
+    {name:"Siddharth" , age:84 , Hsc: 55, gender: "male"},
+    {name:"Rajat" , age:35 , Hsc: 94, gender: "male"},
+    {name:"Ankit" , age:98 , Hsc: 37, gender: "male"},
+    {name:"Aditya" , age:35 , Hsc: 94, gender: "male"},
+    {name:"Abhinav" , age:84 , Hsc: 34, gender: "male"},
+    {name:"Vikram" , age:66 , Hsc: 88, gender: "male"},
+    {name:"Rahul" , age:35 , Hsc: 98, gender: "male"},
+    {name:"Sameer" , age:94 , Hsc: 37, gender: "male"},
+    {name:"Karan" , age:65 , Hsc: 34, gender: "male"},
+    {name:"Deepak" , age:28 , Hsc: 95, gender: "male"}, 
+    {name:"Aishwarya" , age:96 , Hsc: 34, gender: "male"},
+    {name:"Neha" , age:64 , Hsc: 67, gender: "female"},
+    {name:"Priya" , age:36 , Hsc: 35, gender: "female"},
+    {name:"Pooja" , age:45 , Hsc: 5, gender: "female"},
+    {name:"Swati" , age:9 , Hsc: 95, gender: "female"},
+    {name:"Divya" , age:65 , Hsc: 95, gender: "female"},
+    {name:"Aaradhya" , age:39 , Hsc: 49, gender: "female"},
+    {name:"Sneha" , age:22 , Hsc: 46, gender: "female"},
+    {name:"Kavita" , age:47 , Hsc: 37, gender: "female"},
+    {name:"Roshni" , age:20 , Hsc: 38, gender: "female"},
+    {name:"Shruti" , age:99 , Hsc: 66, gender: "female"},
+    {name:"Tanvi" , age:67 , Hsc: 65, gender: "female"},
+    {name:"Gauri" , age:34 , Hsc: 35, gender: "female"},
+    {name:"Meera" , age:20 , Hsc: 20, gender: "female"},
+    {name:"Nisha" , age:37 , Hsc: 91, gender: "female"}
   ]
- 
-
-
+  
 }
 
 
--- src/app/app.pipe.ts
+++ src/app/app.pipe.ts
@@ -1,16 +1,28 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core'; 
 
 @Pipe({
   name: 'app'
 })
-export class AppPipe implements PipeTransform {
+export class AppPipe implements PipeTransform { 
+    transform(value: any[], searchTerm: string, selectedOption: string) { 
+      if (!searchTerm || !selectedOption) {
+        return value; 
+      }  
+      return value.filter((item: any) => { 
+        switch (selectedOption) {
+          case 'age':
+            return item.age.toString().indexOf(searchTerm) > -1;
+          case 'name':
+            return item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+          case 'gender': 
+            // return item.gender.toString().indexOf(searchTerm) > -1;
+            return item.gender.toLowerCase() === searchTerm.toLowerCase();
 
-  transform(value: any, searchTurm: any): any {
-    return value.filter(function(search: any){
-      return search.name.indexOf(searchTurm)> -1
-    }) 
-    
+          case 'hsc':
+            return item.Hsc.toString().indexOf(searchTerm) > -1;
+          default:
+            return false;
+        }
+    });
   }
-
-
 }
